22152868d7cf18598e4c707a54a9f21726ebd07b
xian429429 <camille940429@gmail.com>
2025-11-25 14:30:03 +0800

    Update p4.cpp

--- src/p4.cpp
+++ src/p4.cpp
@@ -1,10 +1,19 @@
-// 題目 4：遞迴的足跡 (Footsteps of Recursion) - 30分
-
 #include <iostream>
 using namespace std;
-
-int main() {
-    
-    
-    return 0;
+int Fib(int i){
+    if(i==1||i==2){
+        return 1;
+    }
+    return Fib(i-1)+Fib(i-2);
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<"SEARCH"<<" "<<"fib("<<n<<")"<<endl;
+    cout<<"SEARCH"<<" "<<"fib("<<n-1<<")"<<endl;
+    cout<<"GET"<<" "<<"fib("<<n-1<<")"<<"="<<Fib(n-1)<<endl;
+    cout<<"SEARCH"<<" "<<"fib("<<n-2<<")"<<endl;
+    cout<<"GET"<<" "<<"fib("<<n-2<<")"<<"="<<Fib(n-2)<<endl;
+    cout<<"GET"<<" "<<"fib("<<n<<")"<<"="<<Fib(n)<<endl;
+    cout<<Fib(n)<<endl;
 }
