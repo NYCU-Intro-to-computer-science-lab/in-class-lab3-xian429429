606a717afb6e37335fe8a6500e27c266d51f3138
xian429429 <camille940429@gmail.com>
2025-11-25 13:53:55 +0800

    Update p2.cpp

--- src/p2.cpp
+++ src/p2.cpp
@@ -1,10 +1,13 @@
-// 題目 2：完美鋪磚計畫 (The Perfect Tiling Project) - 20分
-
 #include <iostream>
 using namespace std;
 
+int gcd(int a, int b) {
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
 int main() {
-    
-    
+    int a, b;
+    cin>>a>>b;
+    cout<<gcd(a,b)<<endl;
     return 0;
 }
