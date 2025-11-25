a8c49de3d59fad8d4b26c2c32258ff5b3af538f7
xian429429 <camille940429@gmail.com>
2025-11-25 14:03:35 +0800

    Update p3.cpp

--- src/p3.cpp
+++ src/p3.cpp
@@ -1,10 +1,24 @@
-// 題目 3：破解數學家的謎題 (Cracking the Mathematician's Puzzle) - 30分
-
 #include <iostream>
 using namespace std;
 
-int main() {
-    
-    
+bool isPrime(int n){
+    if(n<=1) return false;
+    if(n==2) return true;
+    if(n%2==0) return false;
+    for(int i=3;i*i<=n;i=i+2){
+        if(n%i==0) return false;
+}
+    return true;
+}
+int main(){
+    int n;
+    cin>>n;
+    for(int a=2;a<=n/2;a++){
+        int b=n-a;
+        if(isPrime(a)&&isPrime(b)){
+            cout<<a<<" "<<b<<endl;
+            break;
+        }
+    }
     return 0;
 }
